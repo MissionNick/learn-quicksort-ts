53cea7fead98f6c96af4953af0ccc0760db1173b
MissionNick <nickw@missionreadyhq.com>
2022-09-14 00:46:18 +1200

    Complete QuickSort example.  Tutorial - https://www.youtube.com/watch?v=P6XGSKO2RzI

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # learn-quicksort-ts
-Example of a quicksort using typescript jest testing 
+Example of a quicksort using typescript jest testing to validate usecases
+
+
---
+++ jest.config.js
@@ -0,0 +1,22 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    '!**/coverage/**',
+    '!**/*.config.js'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
+};
